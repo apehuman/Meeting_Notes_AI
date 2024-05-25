5480bc673f284acb9f644cdd72bf00f5c96348fd
jj <woojae09@naver.com>
2024-05-26 00:52:20 +0900

    3.1. [front] User Log In & Out pages

--- front/template.py
+++ front/template.py
@@ -2,10 +2,22 @@ import streamlit as st
 
 def base():
     """Parent Template"""
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
 
     with col1:
         st.page_link("index.py", label="Sound Scanner", icon="🏠")
     with col2:
         st.page_link("pages/folders.py", label="Folders", icon="📁")
+    with col3:
+        # init login session state
+        if 'auth' not in st.session_state:
+            st.session_state.auth = False
+        
+        if st.session_state.auth:
+            st.write(f"Hello, {st.session_state.username}")
+            if st.button("Log out"):
+                st.session_state.auth = False
+                st.switch_page("pages/user_logout.py")
+        else:
+            st.page_link("pages/user_login.py", label="Log in")
     st.divider()
